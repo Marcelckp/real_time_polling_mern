3cd3640aed1d1e2467ae11766259f07db59c4d01
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2025-02-10 21:48:57 +0000

    chore: created backend example server code for express server

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 const server = require("./server");
 
 const port = process.env.PORT || 3030;
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log("Server running on port", `${port}`);
+server.listen(port, (err) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.error("Error starting server:", err);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log("Server running on port", `${port}`);
+  }
 });
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,13 +1,46 @@
-const { setUp } = require("./utils/db");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const ErrorHandler = require("./middlewares/error.middleware");
 const Four04Handler = require("./middlewares/404.middleware");
-
 const { app, httpServer } = require("./utils/server");
 
-setUp();
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
+// Routes
+const memData = [
+  {
+    id: 1,
+    time: new Date().toLocaleTimeString(),
+    text: "Time - 1 " + new Date().toLocaleTimeString(),
+  },
+];
+
+const router = express.Router();
+
+setInterval(() => {
+  memData.push({
+    id: memData.length + 1,
+    time: new Date().toLocaleTimeString(),
+    text:
+      "Time - " + (memData.length + 1) + " " + new Date().toLocaleTimeString(),
+  });
+}, 1000);
+
+router.get("/poll", (req, res) => {
+  res.json({ data: memData });
+});
+
+router.get("/test", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/", router);
+
+// Error handling
 app.use(Four04Handler);
-
 app.use(ErrorHandler);
 
 module.exports = httpServer;
